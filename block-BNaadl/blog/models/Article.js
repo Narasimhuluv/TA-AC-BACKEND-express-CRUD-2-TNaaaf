7709e5e0e0fdff94fb2db54ebc6a10b65d8c3317
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-07-19 16:52:52 +0530

    added files

---
+++ block-BNaadl/blog/models/Article.js
@@ -0,0 +1,11 @@
+let mongoose = require('mongoose')
+let Schema  = mongoose.Schema;
+let articleSchema = new Schema({
+    title : String,
+    description : String,
+    tags : [String],
+    author : String,
+    likes : Number
+},{timestamps : true})
+let Article = mongoose.model("Article", articleSchema)
+module.exports = Article;
